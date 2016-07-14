406b45a331afc94c9f579a23c6dd4d7c0fe351db
thanhnt <thanhntgc@gmail.com>
2016-07-14 21:41:18 +0700

    update

--- app/code/local/SmartLab/Datacategory/data/datacategory_setup/data-upgrade-0.1.0-0.1.1.php
+++ app/code/local/SmartLab/Datacategory/data/datacategory_setup/data-upgrade-0.1.0-0.1.1.php
@@ -54,6 +54,37 @@ function addAttributetypevarchar($name, $entityid, $note, $attributeset, $attrib
     $attribute->save();
 }
 
+function addAttributetypevarcharNoRequired($name, $entityid, $note, $attributeset, $attributegroup)       //lam viec voi table eav_attribute va eav_entity_attribute : yeu cau ten entity , id entity (4)
+{
+    $attribute = Mage::getModel('eav/entity_attribute')
+        ->setEntityTypeId($entityid)
+        ->setAttributeCode($name)
+        ->setBackendType("varchar")
+        ->setFrontendInput("text")
+        ->setFrontendLabel($name)
+        ->setIsRequired(0)
+        ->setNote($note)
+        ->setAttributeSetId($attributeset)
+        ->setAttributeGroupId($attributegroup);
+    $attribute->save();
+}
+
+function addAttributetypevarcharUnique($name, $entityid, $note, $attributeset, $attributegroup)       //lam viec voi table eav_attribute va eav_entity_attribute : yeu cau ten entity , id entity (4)
+{
+    $attribute = Mage::getModel('eav/entity_attribute')
+        ->setEntityTypeId($entityid)
+        ->setAttributeCode($name)
+        ->setBackendType("varchar")
+        ->setFrontendInput("text")
+        ->setFrontendLabel($name)
+        ->setIsRequired(1)
+        ->setIsUnique(1)
+        ->setNote($note)
+        ->setAttributeSetId($attributeset)
+        ->setAttributeGroupId($attributegroup);
+    $attribute->save();
+}
+
 function addAttributetypeDate($name, $entityid, $note, $attributeset, $attributegroup)       //lam viec voi table eav_attribute : yeu cau ten entity , id entity (4)
 {
     $attribute = Mage::getModel('eav/entity_attribute')
@@ -70,7 +101,23 @@ function addAttributetypeDate($name, $entityid, $note, $attributeset, $attribute
     $attribute->save();
 }
 
-function addAttributetypeSelect($name, $entityid, $note, $attributeset, $attributegroup)       //lam viec voi table eav_attribute va eav_entity_attribute : yeu cau ten entity , id entity (4)
+function addAttributetypeDateNoRequired($name, $entityid, $note, $attributeset, $attributegroup)       //lam viec voi table eav_attribute : yeu cau ten entity , id entity (4)
+{
+    $attribute = Mage::getModel('eav/entity_attribute')
+        ->setEntityTypeId($entityid)
+        ->setAttributeCode($name)
+        ->setBackendType("datetime")
+        ->setFrontendInput("date")
+        ->setBackendModel("eav/entity_attribute_backend_datetime")
+        ->setFrontendLabel($name)
+        ->setIsRequired(0)
+        ->setNote($note)
+        ->setAttributeSetId($attributeset)
+        ->setAttributeGroupId($attributegroup);
+    $attribute->save();
+}
+
+function addAttributetypeSelect($name, $entityid, $note, $attributeset, $attributegroup, $arrayoption)       //lam viec voi table eav_attribute va eav_entity_attribute : yeu cau ten entity , id entity (4)
 {
     $attribute = Mage::getModel('eav/entity_attribute')
         ->setEntityTypeId($entityid)
@@ -80,11 +127,7 @@ function addAttributetypeSelect($name, $entityid, $note, $attributeset, $attribu
         ->setBackendModel('eav/entity_attribute_backend_array')
         ->setFrontendLabel($name)
         ->setSourceModel("datacategory/source_optionfoodvsbeverages")
-        ->setOption(array('value' => array('optionone' => array('Sony'),
-            'optiontwo' => array('Samsung'),
-            'optionthree' => array('Apple'),
-        )
-        ))
+        ->setOption($arrayoption)
         ->setIsRequired(1)
         ->setNote($note)
         ->setAttributeSetId($attributeset)
@@ -92,19 +135,100 @@ function addAttributetypeSelect($name, $entityid, $note, $attributeset, $attribu
     $attribute->save();
 }
 
-//echo "<pre>";
-//$demo = Mage::getModel('eav/entity_attribute')->getCollection();
-//var_dump($demo);
-//die;
+function addAttributetypeSelectCountry($name, $entityid, $note, $attributeset, $attributegroup)       //lam viec voi table eav_attribute va eav_entity_attribute : yeu cau ten entity , id entity (4)
+{
+    $attribute = Mage::getModel('eav/entity_attribute')
+        ->setEntityTypeId($entityid)
+        ->setAttributeCode($name)
+        ->setBackendType("varchar")
+        ->setFrontendInput("select")
+        ->setSourceModel('catalog/product_attribute_source_countryofmanufacture')
+        ->setFrontendLabel($name)
+        ->setIsRequired(1)
+        ->setNote($note)
+        ->setAttributeSetId($attributeset)
+        ->setAttributeGroupId($attributegroup);
+//    $attribute->setData('source', 'catalog/product_attribute_source_countryofmanufacture');
+    $attribute->save();
+}
+
+function addAttributetypeYesno($name, $entityid, $note, $attributeset, $attributegroup)       //lam viec voi table eav_attribute va eav_entity_attribute : yeu cau ten entity , id entity (4)
+{
+    $attribute = Mage::getModel('eav/entity_attribute')
+        ->setEntityTypeId($entityid)
+        ->setAttributeCode($name)
+        ->setBackendType("int")
+        ->setFrontendInput("boolean")
+        ->setFrontendLabel($name)
+        ->setIsRequired(1)
+        ->setNote($note)
+        ->setAttributeSetId($attributeset)
+        ->setAttributeGroupId($attributegroup);
+    $attribute->save();
+}
+
+function addAttributetypeMulti($name, $entityid, $note, $attributeset, $attributegroup, $arrayoption)       //lam viec voi table eav_attribute va eav_entity_attribute : yeu cau ten entity , id entity (4)
+{
+    $attribute = Mage::getModel('eav/entity_attribute')
+        ->setEntityTypeId($entityid)
+        ->setAttributeCode($name)
+        ->setBackendType("text")
+        ->setFrontendInput("multiselect")
+        ->setBackendModel('eav/entity_attribute_backend_array')
+        ->setFrontendLabel($name)
+        ->setOption($arrayoption)
+        ->setIsRequired(1)
+        ->setNote($note)
+        ->setAttributeSetId($attributeset)
+        ->setAttributeGroupId($attributegroup);
+    $attribute->save();
+}
+
 
 $idset = addAttributeSet("Foods & Beverages", $iddefault);
 $idgroup = addAttributeGroup("Foods & Beverages", $idset);
 addAttributetypevarchar("Calories", $iddefault, "Amount of calories contain inside the food", $idset, $idgroup);
 addAttributetypeDate("Expiry Date", $iddefault, "Date that the food will be expired and cannot be eaten anymore", $idset, $idgroup);
 addAttributetypevarchar("Ingredients", $iddefault, "Details about ingredients used to make that food", $idset, $idgroup);
-addAttributetypeSelect("Food Brand", $iddefault, "The brand of food", $idset, $idgroup);
+addAttributetypeSelect("Food Brand", $iddefault, "select one brand of food", $idset, $idgroup,
+    array('value' => array('optionone' => array('Descendant of the Sun'),
+        'optiontwo' => array('KFC'),
+        'optionthree' => array('McDonald'),
+        'optionfour' => array('Jesus'),
+        'optionfive' => array('Mario Bros'),
+        'optionsix' => array('Pho24'),
+        'optionseven' => array('Teo Kai Chym Singapore'),
+        'optioneight' => array('Phuc Dat Bich'))));
+
+
 $idset = addAttributeSet("Materials", $iddefault);
-addAttributeGroup("Materials", $idset);
+$idgroup = addAttributeGroup("Materials", $idset);
+addAttributetypeSelect("From Farm", $iddefault, "A list of farms provide Tres Bien food material for selling, at this moment", $idset, $idgroup, array('value' => array('optionone' => array('Cooking Recipe'),
+    'optiontwo' => array('Bruce Lee'),
+    'optionthree' => array('Small Ville'),
+    'optionfour' => array('H.K.T'),
+    'optionfive' => array('S.T MTP Vietnam'))));
+addAttributetypeYesno("Is Raw?", $iddefault, "Indicate whether the material is raw or already cooked.", $idset, $idgroup);
 
 
+$idset = addAttributeSet("e-Books", $iddefault);
+$idgroup = addAttributeGroup("e-Books", $idset);
+addAttributetypevarcharUnique("ISBN", $iddefault, "Unique serial number of the e-book", $idset, $idgroup);
+addAttributetypevarchar("Author", $iddefault, "Name of the book’s author", $idset, $idgroup);
+addAttributetypevarcharNoRequired("Publisher", $iddefault, "Name of the book’s publisher", $idset, $idgroup);
+addAttributetypeMulti("Book Type", $iddefault, "A multi-selection of type for the e-book , initially accept the following types", $idset, $idgroup,
+    array('value' => array('optionone' => array('Cooking Recipe'),
+        'optiontwo' => array('Nutrition Guides'),
+        'optionthree' => array('Electronic User Manual'),
+        'optionfour' => array('Chef Biography'),
+        'optionfive' => array('Others'))));
+addAttributetypeDateNoRequired("Date of publishing", $iddefault, "The date when the e-book was published", $idset, $idgroup);
+
+
+$idset = addAttributeSet("Cooking Appliances", $iddefault);
+$idgroup = addAttributeGroup("Cooking Appliances", $idset);
+addAttributetypevarchar("Voltage", $iddefault, "Indicate the number of voltage needed to use the appliance", $idset, $idgroup);
+addAttributetypevarchar("Cooking appliances brand", $iddefault, "The brand of appliance", $idset, $idgroup);
+
+addAttributetypeSelectCountry("Country of Manufacture", $iddefault, "Country that manufactures the appliance, input field appears as drop-down and lists all possible countries.", $idset, $idgroup);
 $installer->endSetup();
