9f5e0748b3073fca1c9f1a4b344e2526cc51604f
Nguyen Duy Thanh <thanhnd1@smartosc.com>
2016-07-15 08:38:56 +0700

    Update Baybanbua shipping method term

--- js/baybanbua/baybanbua.js
+++ js/baybanbua/baybanbua.js
@@ -1,22 +1,5 @@
 document.observe('dom:loaded', function() {
-	// Set shipping baybanbua method is default
-	$('s_method_baybanbua_baybanbua').attr('checked','checked');
-
-	// Set shipping baybanbua term
-	var termForm = '<div style="margin: 10px;">';
-	termForm = '<p class="baybanbua_term" style="margin: 5px; font-style: italic;">'
-	termForm += 'I confirm that I give BayBanBua company permission to deliver ';
-	termForm += 'my items bought at Très Bien. If any of my items are lost, ';
-	termForm += 'broken or malfunctioned during shipping period, ';
-	termForm += 'I will take full responsibility and will not sue either BayBanBua or Très Bien or ';
-	termForm += 'Mr. Khue Nguyen Quang.</p>';
-	termForm += '<p class="baybanbua_term" style="margin: 10px;">';
-	termForm += '<input type="checkbox" id="cb_baybanbua_term">I agree</p></div>';
-	$('s_method_baybanbua_baybanbua').up('li').down('label').insert({ after: termForm });
-
-	// Disable button submit
-	$('shipping-method-buttons-container').down('button')[0].attr('id', 'shipping-method-buttons-submit');
-	$('shipping-method-buttons-submit').disabled = true;
+	$('shipping-method-buttons-container').down('button').removeAttribute('onclick');
 
 	$('checkout-shipping-method-load').on('change','input:radio',function(event){
 		// Check baybanbua shipping method is checked
@@ -28,8 +11,23 @@ document.observe('dom:loaded', function() {
 				$$('.baybanbua_term').each(Element.show);
 				return;
 			}
+			
+			// Baybanbua shipping method is checked and does not exists
+			var termForm = '<div style="margin: 10px;">';
+			termForm = '<p class="baybanbua_term" style="margin: 5px; font-style: italic;">'
+			termForm += 'I confirm that I give BayBanBua company permission to deliver ';
+			termForm += 'my items bought at Très Bien. If any of my items are lost, ';
+			termForm += 'broken or malfunctioned during shipping period, ';
+			termForm += 'I will take full responsibility and will not sue either BayBanBua or Très Bien or ';
+			termForm += 'Mr. Khue Nguyen Quang.</p>';
+			termForm += '<p class="baybanbua_term" style="margin: 10px;">';
+			termForm += '<input type="checkbox" id="cb_baybanbua_term">I agree</p></div>';
+			termForm += '<script>$("cb_baybanbua_term").on("click",function(){';
+			termForm += 'if(!$("cb_baybanbua_term:checked")){$("cb_baybanbua_term").writeAttribute("checked",true);';
+			termForm += 'return;}$("cb_baybanbua_term").rmoveAttribute("checked");});</script>';
 
-			// Disable
+			$(event.target.id).up('li').down('label').insert({ after: termForm });
+			return;
 		}
 		
 		// Another shipping method checked
@@ -37,32 +35,30 @@ document.observe('dom:loaded', function() {
 		{
 			$$('.baybanbua_term').each(Element.hide);
 		}
-
-		$('shipping-method-buttons-submit').disabled = false;
 		
 	});
 
 	$('shipping-method-buttons-container').on('click',function(event){
 		// Check baybanbua_term exist
 		if (!$$('.baybanbua_term').length) {
-			return true;
+			shippingMethod.save();
+			return;
 		}
 
 		// Check baybanbua_term hide
 		if('none' == $$('.baybanbua_term')[0].getStyle('display'))
 		{
-			return true;
+			shippingMethod.save();
+			return;
 		}
 		
 		// Check baybanbua_term which customer agreed
-		if(!$('cb_baybanbua_term:checked'))
+		if(!$('cb_baybanbua_term').getAttribute('checked'))
 		{
 			alert('Must be agree term to continue');
-			Event.stop(event);
-        	event.stopPropagation();
+			return;
 		}
 
-		return true;
-
+		shippingMethod.save();
 	});
 });
