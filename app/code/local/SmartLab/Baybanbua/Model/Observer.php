f3cb0578e7671d12694b0c6f2f8548487e450318
Nguyen Duy Thanh <thanhnd1@smartosc.com>
2016-07-17 23:22:46 +0700

    OT Fix cannot compete order, cannot add baybanbua code. Time: 1h

--- app/code/local/SmartLab/Baybanbua/Model/Observer.php
+++ app/code/local/SmartLab/Baybanbua/Model/Observer.php
@@ -7,28 +7,26 @@ class SmartLab_Baybanbua_Model_Observer
 	function setBaybanbuaCode($observer)
 	{
 		$order = $observer->getEvent()->getOrder();
-		if (!$order->getState() == Mage_Sales_Model_Order::STATE_NEW) {
-			return $this;
-		}
 
-		$order = Mage::getModel('sales/order')->load($order->getId());
-
-		if('baybanbua' == $order->getPayment()->getMethodInstance()->getCode())
-		{
-			// Create random code
-			$alphanumberic = array_merge(range('a','z'),range('A','Z'),range('0','9'));
-			$baybanbua_code = substr(str_shuffle($alphanumberic,0,20));
-
-			// Add code to order
-			$order->setBaybanbuaCode($baybanbua_code);
-			try
+		// Set baybanbua code for new order
+		if ($order->getState() == Mage_Sales_Model_Order::STATE_NEW) {
+			if('baybanbua_baybanbua' == $order->getShippingMethod())
 			{
-				$order->save();
-			} 
-			catch(Exception $e)
-			{
-				Mage::log($e->getMessage());
-				Mage::log($e->getTraceAsString());
+				// Create random code
+				$alphanumberic = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+				$baybanbua_code = substr(str_shuffle($alphanumberic),0,19);
+
+				// Add code to order
+				$order->setBaybanbuaCode($baybanbua_code);
+				try
+				{
+					$order->save();
+				} 
+				catch(Exception $e)
+				{
+					Mage::log($e->getMessage());
+					Mage::log($e->getTraceAsString());
+				}
 			}
 		}
 
--- app/code/local/SmartLab/Megaorder/Model/Observer.php
+++ app/code/local/SmartLab/Megaorder/Model/Observer.php
@@ -6,9 +6,19 @@ class SmartLab_Megaorder_Model_Observer
 {
 	
 	function changeShipmentStatus($observer)
-	{
-		// Change order staus
+	{		
         $orderId = $observer->getEvent()->getData()['controller_action']->getRequest()->getParams()['order_id'];
+
+        // Ignore when invoiced order
+        $orders = Mage::getModel('sales/order_invoice')->getCollection()
+                        ->addAttributeToFilter('order_id', array('eq'=>$orderId));
+        $orders->getSelect()->limit(1);
+
+        if ((int)$orders->count() !== 0) {
+            return $this;
+        }
+
+        // Change order status
         $order = Mage::getModel('sales/order')->load($orderId);
         $order->setStatus('shipped');
         $order->save();
