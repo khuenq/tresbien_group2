0eb37930455d349cdc0a454705c5c8f19c22cbcc
thaohoangf <hoangthuthao24@gmail.com>
2016-07-19 09:34:43 +0700

    ThanhND1 commit from ThaoHT

---
+++ app/code/local/SmartLab/Baybanbua/sql/baybanbua_setup/install-0.1.0.php
@@ -0,0 +1,17 @@
+<?php
+$installer = $this;
+$installer->startSetup();
+
+$attribute= array(
+    'type'          => 'varchar',
+    'label'         => 'Baybanbua Code',
+    'source'        => 'eav/entity_attribute_backend_array',
+    'backend'       => 'eav/entity_attribute_backend_table',
+    'global'        => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+    'visible'       => true,
+    'required'      => false,
+);
+$setup = new Mage_Eav_Model_Entity_Setup('core_setup');
+$setup->addAttribute('order', 'baybanbua_code', $attribute);
+$installer->endSetup();
+?>
