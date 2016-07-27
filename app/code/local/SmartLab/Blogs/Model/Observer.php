7d2e10d78b74655775322337d35c834839bf1e6e
thaohoangf <thaohoangf@users.noreply.github.com>
2016-07-27 23:23:11 +0700

    Update Observer.php

--- app/code/local/SmartLab/Blogs/Model/Observer.php
+++ app/code/local/SmartLab/Blogs/Model/Observer.php
@@ -1,18 +1,24 @@
 <?php
 /**
-* 
-*/
+ * Created by PhpStorm.
+ * User: ThaoHoang
+ * Date: 7/22/2016
+ * Time: 11:06
+ */
+
 class SmartLab_Blogs_Model_Observer
 {
-	
-	function listBlogByNew($observer)
-	{
-		var_dump(1);
-		die();
-		$blogs = $observer->getEvent();
-		echo "<pre>";
-		var_dump($blogs);
-		die();
-	}
+    public function hookIntoReadMore()
+    {
+        $blogId = Mage::app()->getRequest()->getParam('id');
+        $blog = Mage::getModel('neotheme_blog/post')->load($blogId);
+        $tagId = $blog->getTagIds();
+        foreach ($tagId as $id){
+            $tag = Mage::getModel('blogs/tag')->load($id);
+            $index = $tag->getIndex();
+            $index++;
+            $tag->setData('index', $index);
+            $tag->save();
+        }
+    }
 }
-?>
