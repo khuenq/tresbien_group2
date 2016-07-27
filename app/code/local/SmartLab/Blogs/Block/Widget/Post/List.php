392f66231990216f9864af9e4eb76964bc7a3c42
thaohoangf <thaohoangf@users.noreply.github.com>
2016-07-27 23:17:48 +0700

    Update List.php

--- app/code/local/SmartLab/Blogs/Block/Widget/Post/List.php
+++ app/code/local/SmartLab/Blogs/Block/Widget/Post/List.php
@@ -1,1 +1,38 @@
+<?php
+class SmartLab_Blogs_Block_Widget_Post_List
+extends NeoTheme_Blog_Block_Post_List 
+implements Mage_Widget_Block_Interface
+{	
+    function _construct(){
+        $this->setTemplate('smartlab/blogs/widget/post/list_block.phtml');
+        $this->setTitle('Latest Posts');
+    }
+    
+    public function _toHtml() {
+        if ($this->getData('template')){
+            $this->setTemplate($this->getData('template'));
+        }
+        return parent::_toHtml();
+    }
+    
+    public function _prepareCollection(){
+        
+        parent::_prepareCollection();
+        if (is_numeric($this->getPostCount())){
+            $this->getCollection()->setCurPage(0);
+            $this->getCollection()->setPageSize($this->getPostCount());
+        }
+    }
 
+    public function getLastestBlog()
+    {
+        $store_id = Mage::app()->getStore()->getId();
+        $blogModel = Mage::getModel('neotheme_blog/post')->getCollection()->addFieldToFilter('store_ids', array('in'=>$store_id));
+        $blogModel->getSelect()->order(array('entity_id DESC'));
+        $blog = array();
+        foreach ($blogModel as $item){
+            array_push($blog, $item);
+        }
+        return $blog;
+    }
+}
