b1807e1f3a48f62249d7b2ab99e0e22321da9910
thaohoangf <thaohoangf@users.noreply.github.com>
2016-07-27 23:00:54 +0700

    Update Edit.php

--- app/code/local/SmartLab/Blogs/Block/Account/Myblog/Edit.php
+++ app/code/local/SmartLab/Blogs/Block/Account/Myblog/Edit.php
@@ -2,45 +2,103 @@
 /**
  * Created by PhpStorm.
  * User: ThaoHoang
- * Date: 7/14/2016
- * Time: 14:13
+ * Date: 7/11/2016
+ * Time: 5:28 PM
  */
-class SmartLab_Blogs_Block_Account_Myblog_Edit extends Mage_Core_Block_Template
+class SmartLab_Blogs_Block_Account_Myblog_List
+extends Mage_Core_Block_Template
 {
+//    construct function
     public function _construct()
     {
         parent::_construct();
-        $collection = $this->getCategoryCollection();
+        $this->setTitle('List Blog');
+        $collection = $this->getPostCollection();
         $this->setCollection($collection);
     }
 
-    function getPost(){
-        $id = Mage::app()->getRequest()->getParam('id');
-        return  Mage::getModel('neotheme_blog/post')->load($id);
-    }
-
-    public function getCategoryCollection()
+//  prepare layout
+    public function _prepareLayout()
     {
-        $store_id = Mage::app()->getStore()->getId();
-        $collection = Mage::getModel('neotheme_blog/category')->getCollection()->addFieldToFilter('store_ids',$store_id);
-        return $collection;
-    }
-
-    public function getListTag()
-    {
-        $modelTag = Mage::getModel('blogs/tag');
-        $listTagName = array();
-        $id = Mage::app()->getRequest()->getParam('id');
-        $listTagById = Mage::getModel('neotheme_blog/post')->load($id)->getTagIds();
-        foreach ($listTagById as $tagId){
-            $tagName = $modelTag->load($tagId)->getName();
-            array_push($listTagName,$tagName);
+        if($this->getPostCollection() != 0) {
+            parent::_prepareLayout();
+            $pager = $this->getLayout()->createBlock('page/html_pager', 'blog.pager')
+                ->setCollection($this->getCollection());
+            $this->setChild('page', $pager);
+            return $this;
         }
-        $listTag =implode(',', $listTagName);
-        return $listTag;
     }
-    public function getActionOfForm()
+
+
+    public function getPostCollection() {
+        //Hien thi blog theo id
+        //Lay id cua customer
+        $customerId = Mage::getSingleton('customer/session')->getCustomerId();
+        //Lay cac id cua blog theo id customer
+        $post = Mage::getModel('blogs/customerpost')->getCollection()->addFieldToFilter('customer_id', $customerId);
+        if($post->count() == 0){
+            return 0;
+        }else{
+            $blogId = array();
+            foreach ($post as $item){
+                array_push($blogId,$item->getPostId());
+            }
+            $collection = Mage::getModel('neotheme_blog/post')->getCollection()->addFieldToFilter('entity_id', $blogId);
+            return $collection;
+        }
+       
+    }
+
+    public function getPagerHtml()
     {
-        return $this->getUrl('blogs/index/edit');
+        return $this->getChildHtml('page');
+    }
+
+    public function getStatusLabel($blog)
+    {
+        if($blog->getId()){
+            $status = $blog->getStatus();
+           switch ($status){
+               case 0:
+                   return Mage::helper('blogs')->__('Inactive');
+                   break;
+               case 1:
+                   return Mage::helper('blogs')->__('Active');
+                   break;
+               case 2:
+                   return Mage::helper('blogs')->__('Draft');
+                   break;
+           }
+        }
+    }
+
+    public function getCategoryLabel($blog)
+    {
+        if($blog->getId()){
+            $category_id = $blog->getData('category_ids');
+            $categoryName = '';
+            $category = Mage::getModel('neotheme_blog/category')->getCollection();
+            foreach ($category as $item) {
+                if($category_id == $item->getData('entity_id')){
+                    $categoryName = $item->getName();
+                }
+            }
+            return $categoryName;
+        }
+    }
+
+    public function getStoreLabel($blog)
+    {
+        if($blog->getId()){
+            $store_id = $blog->getData('store_ids');
+            $storeName  = '';
+            $store = Mage::getModel('core/store')->getCollection();
+            foreach ($store as $item) {
+                if($store_id == $item->getData('store_id')){
+                    $storeName = $item->getName();
+                }
+            }
+        }
+        return $storeName;
     }
 }
